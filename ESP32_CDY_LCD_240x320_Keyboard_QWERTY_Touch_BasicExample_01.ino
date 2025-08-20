ff3fbc3eef8771868c4f782e78a18d93bbd8a2d3
FelonyFelyne <medicalsystemslab@gmail.com>
2025-08-20 13:36:42 -0600

    Agregada la configuracion de la pantalla CDY

--- ESP32_CDY_LCD_240x320_Keyboard_QWERTY_Touch_BasicExample_01.ino
+++ ESP32_CDY_LCD_240x320_Keyboard_QWERTY_Touch_BasicExample_01.ino
@@ -1,5 +1,9 @@
 //Libreria de la pantalla 
     // ( no olvidar configurar correctamente el archivo: User_Setup.h de la libreria )
+    // (se agrega la configuracion de pines para el CDY al final de este main, solo inicialmente, despues se va a remover.)
+
+
+
 #include <TFT_eSPI.h>               //https://github.com/Bodmer/TFT_eSPI
 #include <XPT2046_Touchscreen.h>    //https://github.com/PaulStoffregen/XPT2046_Touchscreen
 
@@ -77,3 +81,73 @@ void loop() {
 paginas_Principal(); // revision de pagina en curso a desplegar
 
 } //fin de void loop()
+
+
+
+
+
+
+
+
+//Este es el que debe funcionar bien para su CDY, el V3, pero abajo tambien hay otro para el V1 y el V2
+//Reemplazar lo necesario de esto en el User_Setup.h de la libreria TFT_eSPI
+
+/*
+
+// CDY V3, version YELLOW ESP32 DISPLAY 2.8"    
+//#define ST7789_DRIVER
+#define ILI9341_2_DRIVER
+#define TFT_RGB_ORDER TFT_BGR 
+
+#define TFT_INVERSION_ON //algunas pantallas no necesitan esto
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
+
+#define TFT_BL   21
+#define TFT_BACKLIGHT_ON HIGH  //DESACTIVAR SI SE USA CONTROL DE LUMINOSIDAD EN PIN TFT_BL
+
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15  // Chip select control pin
+#define TFT_DC    2  // Data Command control pin
+#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
+
+#define TOUCH_CS 33     // Chip select pin (T_CS) of touch screen
+#define TOUCH_MOSI 32 // Separate Touch SoftSPI bus
+#define TOUCH_MISO 39 // Separate Touch SoftSPI bus
+#define TOUCH_CLK 25  // Separate Touch SoftSPI bus
+#define USE_HSPI_PORT  //Importante sin esta linea el touch no funciona
+
+*/
+
+
+/*
+  
+// CDY V1, V2, version YELLOW ESP32 DISPLAY 2.8"
+#define ILI9341_2_DRIVER   
+//#define TFT_INVERSION_ON //algunas pantallas no necesitan esto
+//#define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
+
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
+
+#define TFT_BL   21
+#define TFT_BACKLIGHT_ON HIGH   //DESACTIVAR SI SE USA CONTROL DE LUMINOSIDAD EN PIN TFT_BL
+
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15  // Chip select control pin
+#define TFT_DC    2  // Data Command control pin
+#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
+
+#define TOUCH_CS 33     // Chip select pin (T_CS) of touch screen
+#define TOUCH_MOSI 32 // Separate Touch SoftSPI bus
+#define TOUCH_MISO 39 // Separate Touch SoftSPI bus
+#define TOUCH_CLK 25  // Separate Touch SoftSPI bus
+#define USE_HSPI_PORT  //Importante sin esta linea el touch no funciona
+
+*/
